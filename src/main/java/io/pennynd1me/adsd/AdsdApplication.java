4dcf007c365081252751b538ba896183c19add7c
pennynd1me <4prilkim@gmail.com>
2024-04-08 17:21:40 +0900

    프로젝트 초기 설정
    Java 17
    Spring Boot 3.2.4
    Gradle 8.7

---
+++ src/main/java/io/pennynd1me/adsd/AdsdApplication.java
@@ -0,0 +1,13 @@
+package io.pennynd1me.adsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/pennynd1me/adsd/AdsdApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pennynd1me.adsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
